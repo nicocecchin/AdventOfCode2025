eb76e89908380275ae58348434562d7e75b19ce5
StefanoGenettiUniTN <stefanogenetti2000@gmail.com>
2025-12-01 22:37:05 +0100

    problem 1

--- day01/main.cpp
+++ day01/main.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main() {
+    ifstream inputFile("day01/input.txt");
+    ofstream outputFile("day01/output.txt");
+    int result = 0;
+    string rotation;
+    int zeroCounter = 0;
+    int currentPos = 50;
+    char direction;
+    int delta;
 
-    cout<<"Hello, World!"<<endl;
+    while (getline(inputFile, rotation)) {
+        direction = rotation[0];
+        delta = stoi(rotation.erase(0,1));
+        
+        if(direction == 'R'){
+            currentPos = (currentPos+delta)%100;
+        }else{
+            currentPos = (currentPos-delta)%100;
+        }
 
+        if(currentPos==0){
+            zeroCounter++;
+        }
+    }
+
+    result = zeroCounter;
+    outputFile << result << endl;
+    outputFile.close();
     return 0;
-
 }
